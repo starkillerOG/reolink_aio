dcdfef6ed52ae9b76716e5520081cc7bc32c97e6
starkillerOG <starkiller.og@gmail.com>
2023-05-12 18:04:38 +0200

    Add PullPoint xmls

--- reolink_aio/templates.py
+++ reolink_aio/templates.py
@@ -78,3 +78,51 @@ UNSUBSCRIBE_XML = """
         </soap:Body>
     </soap:Envelope>
 """
+
+
+PULLPOINT_XML = """
+    <soap:Envelope xmlns:add="http://www.w3.org/2005/08/addressing" xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:wsdl="http://www.onvif.org/ver10/events/wsdl">
+        <soap:Header>
+            <add:Action>http://www.onvif.org/ver10/events/wsdl/EventPortType/CreatePullPointSubscriptionRequest</add:Action>
+            <wsse:Security soap:mustUnderstand="true" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"
+                xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
+                <wsse:UsernameToken wsu:Id="UsernameToken-{UsernameToken}">
+                    <wsse:Username>{Username}</wsse:Username>
+                    <wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordDigest">{PasswordDigest}</wsse:Password>
+                    <wsse:Nonce EncodingType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary">{Nonce}</wsse:Nonce>
+                    <wsu:Created>{Created}</wsu:Created>
+                </wsse:UsernameToken>
+            </wsse:Security>
+        </soap:Header>
+        <soap:Body>
+            <wsdl:CreatePullPointSubscription>
+                <wsdl:InitialTerminationTime>{InitialTerminationTime}</wsdl:InitialTerminationTime>
+            </wsdl:CreatePullPointSubscription>
+        </soap:Body>
+    </soap:Envelope>
+"""
+
+
+PULLMESSAGE_XML = """
+    <soap:Envelope xmlns:add="http://www.w3.org/2005/08/addressing" xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:wsdl="http://www.onvif.org/ver10/events/wsdl">
+        <soap:Header>
+            <add:Action>http://www.onvif.org/ver10/events/wsdl/PullPointSubscription/PullMessagesRequest</add:Action>
+            <add:To>{To}</add:To>
+            <wsse:Security soap:mustUnderstand="true" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"
+                xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
+                <wsse:UsernameToken wsu:Id="UsernameToken-{UsernameToken}">
+                    <wsse:Username>{Username}</wsse:Username>
+                    <wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordDigest">{PasswordDigest}</wsse:Password>
+                    <wsse:Nonce EncodingType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary">{Nonce}</wsse:Nonce>
+                    <wsu:Created>{Created}</wsu:Created>
+                </wsse:UsernameToken>
+            </wsse:Security>
+        </soap:Header>
+        <soap:Body>
+            <wsdl:PullMessages>
+                <wsdl:Timeout>PT1M</wsdl:Timeout>
+                <wsdl:MessageLimit>100</wsdl:MessageLimit>
+            </wsdl:PullMessages>
+        </soap:Body>
+    </soap:Envelope>
+"""
