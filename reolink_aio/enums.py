92c04d2d41cbe88771aed87a92519ae7f55f69f4
starkillerOG <starkiller.og@gmail.com>
2023-10-31 16:27:42 +0100

    add extra typing

--- reolink_aio/enums.py
+++ reolink_aio/enums.py
@@ -10,10 +10,10 @@ class SubType(str, Enum):
     long_poll = "long_poll"
     all = "all"
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.value
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.value
 
 
