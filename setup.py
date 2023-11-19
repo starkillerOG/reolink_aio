5fea716ef995bff5e74124cf1fe6f221a5fa9851
starkillerOG <starkiller.og@gmail.com>
2023-11-19 15:59:48 +0100

    Revert "Revert "Add Python 3.12 test support""
    This reverts commit d08a7aeee4b6ca9ad5ab1323b0ab96e420e0c3ba.

--- setup.py
+++ setup.py
@@ -41,4 +41,5 @@ setup(name='reolink_aio',
           "Programming Language :: Python :: 3.9",
           "Programming Language :: Python :: 3.10",
           "Programming Language :: Python :: 3.11",
+          "Programming Language :: Python :: 3.12",
           ])
