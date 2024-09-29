7ce490ad33e28cc4889d27562a513d2f6a28710d
starkillerOG <starkiller.og@gmail.com>
2024-09-29 09:59:08 +0200

    correct ipcChnBit

--- reolink_aio/api.py
+++ reolink_aio/api.py
@@ -2554,7 +2554,7 @@ class Host:
                 if channel is None:
                     ipcChnBit = 1
                 else:
-                    ipcChnBit = channel + 2
+                    ipcChnBit = 2^channel
                 param["ipcChnBit"] = str(ipcChnBit)
                 param["fileSize"] = len_pak
             body = [{"cmd": "UpgradePrepare", "action": 0, "param": param}]
