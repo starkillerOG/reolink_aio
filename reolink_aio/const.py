2d9fab2b1f7901bb8dbb482a4da1cbed8c9454d9
starkillerOG <starkiller.og@gmail.com>
2026-01-24 13:59:57 +0100

    Add YOLO AI sub_types "child", "van", "truck", "squirrel", "fox", "bear", "cow"

--- reolink_aio/const.py
+++ reolink_aio/const.py
@@ -66,9 +66,9 @@ AI_DETECT_CONVERSION = {"person": "people", "pet": "dog_cat"}
 YOLO_CONVERSION = {"person": "people", "motor vehicle": "vehicle", "animal": "dog_cat"}
 YOLO_DETECTS = {"people", "vehicle", "package", "non-motor vehicle", "dog_cat"}
 YOLO_DETECT_TYPES = {
-    "people": ["man", "woman"],
-    "vehicle": ["sedan", "suv", "pickup_truck", "bus", "motorcycle"],
-    "dog_cat": ["dog", "cat"],
+    "people": ["man", "woman", "child"],
+    "vehicle": ["sedan", "suv", "pickup_truck", "bus", "van", "truck", "motorcycle"],
+    "dog_cat": ["dog", "cat", "squirrel", "fox", "bear", "cow"],
     "package": ["package"],
     "non-motor vehicle": ["bicycle"],
 }
