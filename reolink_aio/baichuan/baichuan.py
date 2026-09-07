e0567b14e76e07308fcdd67f422acd4b3e2f4d47
starkillerOG <starkiller.og@gmail.com>
2026-09-07 17:41:14 +0200

    Allow baichuan_only to be reset to False indicated by the "netPort" capability

--- reolink_aio/baichuan/baichuan.py
+++ reolink_aio/baichuan/baichuan.py
@@ -1873,8 +1873,7 @@ class Baichuan:
                 self._abilities.setdefault(None, {})[None] = support
 
             # check if HTTP(s) API is supported
-            if (self.api_version("netPort", no_key_return=55) & 0x3F) <= 1:
-                self.http_api.baichuan_only = True
+            self.http_api.baichuan_only = (self.api_version("netPort", no_key_return=55) & 0x3F) <= 1
 
         # Host capabilities
         self.capabilities.setdefault(None, {}).setdefault(None, set())
